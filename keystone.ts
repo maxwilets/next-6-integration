bda8748890464a9956f6aee41b8241d112479354
maxwilets <mwilets@gmail.com>
2023-05-14 13:08:41 -0600

    testing env git push

--- keystone.ts
+++ keystone.ts
@@ -27,7 +27,7 @@ export default withAuth(
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: 'mysql',
-      url: `${process.env.DATABASE_URL}`,
+      url: process.env.DATABASE_URL,
     },
     lists,
     session,
