ed892877eb15471c1467cddecd9580c181661dec
maxwilets <mwilets@gmail.com>
2023-05-14 13:16:58 -0600

    hard coding url to test env

--- keystone.ts
+++ keystone.ts
@@ -27,7 +27,7 @@ export default withAuth(
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: 'mysql',
-      url: process.env.DATABASE_URL,
+      url: "db-mysql-nyc1-20804-do-user-14080817-0.b.db.ondigitalocean.com",
     },
     lists,
     session,
